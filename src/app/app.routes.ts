7b95837cf46cd97f39f17e725e035f700c492edb
tkarakok <karakoktunahan78@gmail.com>
2023-11-22 21:27:45 +0300

    design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { BookComponent } from './components/books/book/book.component';
 import { RegisterComponent } from './components/register/register/register.component';
 import { LoginComponent } from './components/login/login/login.component';
+import { CategoryComponent } from './components/category/category/category.component';
+import { AuthorComponent } from './components/author/author/author.component';
 
 export const routes: Routes = [
     {path:"", pathMatch:"full", component:LoginComponent},
     {path:"login", component:LoginComponent},
     {path:"books", component:BookComponent},
     {path:"register", component:RegisterComponent},
+    {path:"categories", component:CategoryComponent},
+    {path:"authors", component:AuthorComponent},
 ];
--- src/app/components/navi/navi/navi.component.ts
+++ src/app/components/navi/navi/navi.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BookComponent } from '../../books/book/book.component';
+import { RouterOutlet } from '@angular/router';
+import { AuthService } from '../../../service/auth.service';
 
 @Component({
   selector: 'app-navi',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BookComponent, RouterOutlet],
   templateUrl: './navi.component.html',
   styleUrl: './navi.component.css'
 })
 export class NaviComponent {
+  authservice : AuthService;
 
+  constructor(private authSer : AuthService){ this.authservice = authSer}
 }
